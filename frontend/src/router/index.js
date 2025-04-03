e7d9f3f561b9776c7492251ec2d872749e3ffdc3
Prasidha R <32433336+prasidha1@users.noreply.github.com>
2025-04-04 05:24:12 +0530

    Login page_placeholder
    Added Login page placeholder

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/", component: HomeView },
+        
+        { path: "/", component:  HomeView},
+        { path: "/Login", name: 'Login', component:  LoginView}
     ],
+
+
 });
+
 
 export default router;
