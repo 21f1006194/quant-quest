6d4426db0f0140768f2f9a9a799421c8dc1ccddc
Arshini Aswath <arshini.aswath@gmail.com>
2025-04-06 20:19:21 +0530

    Updated __init__.py file

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,21 +1,15 @@
-#from .user import User
-from .user2 import User, Role, UserRole
+from .user import User
 from .verification import VerificationToken
 from .profile import UserProfile
 from .wallet import Wallet, Transaction
-from .audit import AuditLog, AdminActivityLog
 from .gameplay import Game, GameSession, Bet 
 
 __all__ = [
     "User",
-    "Role",
-    "UserRole",
     "VerificationToken",
     "UserProfile",
     "Wallet",
     "Transaction",
-    "AuditLog",
-    "AdminActivityLog",
     "Game",
     "GameSession",
     "Bet"
