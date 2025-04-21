fa46dc8e118b2ad2000eb9eeaa549d1b596219f2
jishnujp <jishnujprksh007@gmail.com>
2025-04-21 19:50:13 +0530

    Add AboutView component and update router configuration
    - Introduced a new AboutView component to provide information about the Quantix Club and its flagship event, Quant Quest.
    - Updated the router configuration to include a route for the AboutView, enhancing site navigation and user experience.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
 import LoginView from "../views/LoginView.vue";
 import Register from '../views/Register.vue';
 import AdminDash from '../views/admin/AdminDash.vue';
@@ -12,6 +13,7 @@ const router = createRouter({
         { path: "/admin", name: 'Admin', component: AdminDash },
         { path: "/player", name: 'Player', component: PlayerDash },
         { path: "/", component: HomeView },
+        { path: "/about", component: AboutView },
         { path: "/login", name: 'Login', component: LoginView },
         { path: '/register', name: 'Register', component: Register }
 
