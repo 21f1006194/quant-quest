56b664356772d68ef8e5e9790226b6e0ba9e8125
Prasidha R <32433336+prasidha1@users.noreply.github.com>
2025-04-24 07:34:43 +0000

    merged routes

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,6 +1,3 @@
-from .common import common_bp
 from .admin import admin_bp
-from .player import player_bp
-from .game_router import game_bp, play_bp
 
-__all__ = ["common_bp", "admin_bp", "player_bp", "game_bp", "play_bp"]
+__all__.append("admin_bp")
