fc891767caa93f2603b693e49413363dbdcf7479
jishnujp <jishnujprksh007@gmail.com>
2025-05-09 22:38:02 +0530

    Refactor game registration and initialization logic
    - moved game migrations and db upgrading to a different script in init_db.py instead of keeping it in the app creation and making loading heavy
    - Otherwise gunicorn workers get timeout because of the time it takes to add games to db.

--- backend/run.py
+++ backend/run.py
@@ -4,7 +4,7 @@ import os
 app = create_app()
 
 
-initialize_app(app)
-
 if __name__ == "__main__":
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        initialize_app(app)
     app.run(host="0.0.0.0", port=5000, debug=True)
